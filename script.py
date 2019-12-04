17a1944ce1c2390a148702659d5987636d491e11
vinay-easwaran <vinayeaswaran@yahoo.co.in>
2019-12-03 17:39:14 -0800

    Added input parsing

--- script.py
+++ script.py
@@ -3,31 +3,12 @@ import urllib.request
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
-
-def extractAuthor(gitlog):
-    searchQuery = "Author: "
-    for log in gitlog:
-        if searchQuery in log:
-            beginningIndex = log.index(searchQuery) + len(searchQuery)
-            endIndex = beginningIndex + 1
-            while(log[endIndex] != " "):
-                endIndex += 1
-            return log[beginningIndex:endIndex]
-        
-def extractProjectName(path):
-    x = path.find('/')
-    while(x != -1):
-        path = path[x+1:]
-        x = path.find('/')
-    return path
   
 def extractCommitHashCodes(gitlog: list):
     commits = list()
-    searchQuery = "commit "
     for log in gitlog:
-        if searchQuery in log:
-            beginningIndex = log.index(searchQuery) + len(searchQuery)
-            commits.append(log[beginningIndex:])
+        if log[0:7] == 'commit ':
+            commits.append(log[7:])
             
     return commits
 
@@ -41,33 +22,42 @@ def extractTimeStamps(gitlog):
             
     return timestamps
 
-def constructURL(author, project_name, commit):
-    url = "http://github.com/" + author + "/" + project_name + "/commit/" + commit
+def extractProjectName(path):
+    x = path.find('/')
+    while(x != -1):
+        path = path[x+1:]
+        x = path.find('/')
+    return path
+
+def constructURL(base_url, commit):
+    url = base_url + "/commit/" + commit
     return url
             
 def run(path):
     path_to_repo = path
     g = git.Repo(path_to_repo)
     gitlog = g.git.log(p=True).split("\n")
+    remote_origin = g.git.remote(v=True)
+    base_url = remote_origin.split("\t")[1].split(" ")[0][:-4]
     
-    
-    author = extractAuthor(gitlog)
-    project_name = extractProjectName(path_to_repo)
     commits = extractCommitHashCodes(gitlog)
     time_stamps = extractTimeStamps(gitlog)
+    project_name = extractProjectName(base_url)
+    print("ON PROJECT " + project_name)
     
     urls = list()
     for commit in commits:
-        urls.append(constructURL(author,project_name,commit))
+        urls.append(constructURL(base_url,commit))
     
     commit_messages = list()
     for url in urls:
+        print(url)
         html_doc = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(html_doc, 'html.parser')
         commit_messages.append(soup.title.text)
     
     table = zip(time_stamps,commits,commit_messages,urls)
-    filename = "Output/" + project_name + ".csv"
+    filename = "output/" + project_name + ".csv"
     with open(filename, 'w') as output_file:
         output_writer = csv.writer(output_file,delimiter=",",quotechar='"', quoting=csv.QUOTE_MINIMAL)
         output_writer.writerow(['Time','Hashcode','Commit Title','URL'])
@@ -75,9 +65,10 @@ def run(path):
             output_writer.writerow(row)
         
 if __name__ == '__main__':
-    input_file_path = input("Enter link to project paths")
-    repo_list = input_file_path.readlines()
+    input_file_path = input("Enter link to project paths: ")
+    f = open(input_file_path,"r")
+    repo_list = f.readlines()
+    f.close()
     for repo in repo_list:
-        repo.strip("\n")
+        repo = repo.strip("\n")
         run(repo)
-#     run("C:/Users/Vinay_2/Documents/Mondego/ByteCodeAnalyzer")     
